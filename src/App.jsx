c99151e1a2017372578fb448ffa675921b4f7411
BadisBelkessam <belkessambadis40@gmail.com>
2025-10-21 23:07:46 +0100

    added my picture, my interests in the member data, and the route to my function in the app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import TeamMember from './pages/TeamMember'
 import './App.css'
 import AbderraoufPage from "./members/abderraouf/MemberPage";
+import BadisPage from "./members/badis/MemberPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Hero />} />
           <Route path="/member/abderraouf" element={<AbderraoufPage/>} />
+          <Route path="/member/badis" element={<BadisPage/>} />
         </Routes>
       </div>
     </Router>
