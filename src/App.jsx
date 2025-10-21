fb82d03cb8c1e0a83401f8905594d1ab933a49a3
sadjeda <145927854+sadjeda@users.noreply.github.com>
2025-10-21 22:44:36 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
 import TeamMember from './pages/TeamMember'
 import './App.css'
-import AbderraoufPage from "./members/abderraouf/MemberPage";
 import SadjedaPage from "./members/sadjeda/MemberPage";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Hero />} />
-          <Route path="/member/abderraouf" element={<AbderraoufPage />} />
           <Route path="/member/sadjeda" element={<SadjedaPage/>} />
 
         </Routes>
